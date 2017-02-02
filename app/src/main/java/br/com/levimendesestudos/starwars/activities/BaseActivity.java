868e3f5aacf7e136b9978fcf8848f20080527bc6
429317980 <levi.silva.mendes@gmail.com>
2017-02-02 12:50:31 -0200

    initial commit

---
+++ app/src/main/java/br/com/levimendesestudos/starwars/activities/BaseActivity.java
@@ -0,0 +1,11 @@
+package br.com.levimendesestudos.starwars.activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 809778 on 02/02/2017.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+
+}
---
+++ app/src/main/java/br/com/levimendesestudos/starwars/activities/MainActivity.java
@@ -0,0 +1,13 @@
+package br.com.levimendesestudos.starwars.activities;
+
+import android.os.Bundle;
+import br.com.levimendesestudos.starwars.R;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
