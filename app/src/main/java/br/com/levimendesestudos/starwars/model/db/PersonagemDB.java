81c6a5f8e4cc0daf7661688b55292f62aab7792e
429317980 <levi.silva.mendes@gmail.com>
2017-02-02 16:34:20 -0200

    initial commit da classe que vai manipular os dados na tabela de personagens

---
+++ app/src/main/java/br/com/levimendesestudos/starwars/model/db/PersonagemDB.java
@@ -0,0 +1,15 @@
+package br.com.levimendesestudos.starwars.model.db;
+
+/**
+ * Created by 809778 on 02/02/2017.
+ */
+
+public class PersonagemDB {
+
+    public static final String TB_NAME   = "TB_PERSONAGEM";
+
+    public static final String NOME      = "NOME";
+    public static final String LINK      = "LINK";
+    public static final String LATITUDE  = "LATITUDE";
+    public static final String LONGITUDE = "LONGITUDE";
+}
