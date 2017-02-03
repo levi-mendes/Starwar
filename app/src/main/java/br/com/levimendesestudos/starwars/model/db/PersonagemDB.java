75da8c9004d7aa44a4e90c8d3e5087746d2bd861
429317980 <levi.silva.mendes@gmail.com>
2017-02-03 17:31:45 -0200

    adicionado os novos campos nos métodos de inserir e listar

--- app/src/main/java/br/com/levimendesestudos/starwars/model/db/PersonagemDB.java
+++ app/src/main/java/br/com/levimendesestudos/starwars/model/db/PersonagemDB.java
@@ -7,6 +7,7 @@ import android.database.Cursor;
 import java.util.ArrayList;
 import java.util.List;
 
+import br.com.levimendesestudos.starwars.model.Filme;
 import br.com.levimendesestudos.starwars.model.Personagem;
 
 /**
@@ -17,20 +18,42 @@ public class PersonagemDB extends DBGenericClass {
 
     public static final String TB_NAME   = "TB_PERSONAGEM";
 
-    public static final String NOME      = "NOME";
-    public static final String LINK      = "LINK";
-    public static final String LATITUDE  = "LATITUDE";
-    public static final String LONGITUDE = "LONGITUDE";
+    public static final String NOME       = "NOME";
+    public static final String LINK       = "LINK";
+    public static final String LATITUDE   = "LATITUDE";
+    public static final String LONGITUDE  = "LONGITUDE";
+    public static final String HEIGHT     = "HEIGHT";
+    public static final String MASS       = "MASS";
+    public static final String HAIR_COLOR = "HAIR_COLOR";
+    public static final String SKIN_COLOR = "SKIN_COLOR";
+    public static final String EYE_COLOR  = "EYE_COLOR";
+    public static final String BIRTH_YEAR = "BIRTH_YEAR";
+    public static final String GENDER     = "GENDER";
+    public static final String CREATED    = "CREATED";
+    public static final String EDITED     = "EDITED";
+
+    private Context mContext;
 
     public PersonagemDB(Context context) {
         super(context);
+
+        mContext = context;
     }
 
     public boolean inserir(Personagem p) {
         ContentValues values = new ContentValues();
 
-        values.put(NOME, p.nome);
-        values.put(LINK, p.link);
+        values.put(NOME,       p.name);
+        values.put(LINK,       p.link);
+        values.put(HEIGHT,     p.height);
+        values.put(MASS,       p.mass);
+        values.put(HAIR_COLOR, p.hairColor);
+        values.put(SKIN_COLOR, p.skinColor);
+        values.put(EYE_COLOR,  p.eyeColor);
+        values.put(BIRTH_YEAR, p.birthYear);
+        values.put(GENDER,     p.gender);
+        values.put(CREATED,    p.created);
+        values.put(EDITED,     p.edited);
 
         boolean retorno = database.insert(TB_NAME, null, values) > 0;
 
@@ -41,11 +64,25 @@ public class PersonagemDB extends DBGenericClass {
         List<Personagem> retorno = new ArrayList<>();
         Cursor cursor = database.query(TB_NAME, null, null, null, null, null, null);
 
+        FilmeDB filmeDB = new FilmeDB(mContext);
+
         while (cursor.moveToNext()) {
             Personagem p = new Personagem();
 
-            p.nome = cursor.getString(cursor.getColumnIndex(NOME));
+            p.name = cursor.getString(cursor.getColumnIndex(NOME));
             p.link = cursor.getString(cursor.getColumnIndex(LINK));
+
+            p.height    = cursor.getInt(cursor.getColumnIndex(HEIGHT));
+            p.mass      = cursor.getInt(cursor.getColumnIndex(MASS));
+            p.hairColor = cursor.getString(cursor.getColumnIndex(HAIR_COLOR));
+            p.skinColor = cursor.getString(cursor.getColumnIndex(SKIN_COLOR));
+            p.eyeColor  = cursor.getString(cursor.getColumnIndex(EYE_COLOR));
+            p.birthYear = cursor.getString(cursor.getColumnIndex(BIRTH_YEAR));
+            p.gender    = cursor.getString(cursor.getColumnIndex(GENDER));
+            p.created   = cursor.getString(cursor.getColumnIndex(CREATED));
+            p.edited    = cursor.getString(cursor.getColumnIndex(EDITED));
+
+            //List<Filme> films = filmeDB.buscarFilmes()
 
             retorno.add(p);
         }
