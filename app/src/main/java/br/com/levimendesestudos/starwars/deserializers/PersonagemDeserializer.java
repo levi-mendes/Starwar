3cab87d4c58e1d073cb2bdf0dc3739377b32f9b1
429317980 <levi.silva.mendes@gmail.com>
2017-02-03 17:32:33 -0200

    novos campos

--- app/src/main/java/br/com/levimendesestudos/starwars/deserializers/PersonagemDeserializer.java
+++ app/src/main/java/br/com/levimendesestudos/starwars/deserializers/PersonagemDeserializer.java
@@ -23,8 +23,18 @@ public class PersonagemDeserializer implements JsonDeserializer<Object> {
         Personagem retorno = new Personagem();
         List<Filme> filmes = new ArrayList<>();
         JsonObject root = json.getAsJsonObject();
-        retorno.nome = root.get("name").getAsString();
-        retorno.link = root.get("url").getAsString();
+
+        retorno.name      = root.get("name").getAsString();
+        retorno.link      = root.get("url").getAsString();
+        retorno.height    = root.get("height").getAsInt();
+        retorno.mass      = root.get("mass").getAsInt();
+        retorno.hairColor = root.get("hair_color").getAsString();
+        retorno.skinColor = root.get("skin_color").getAsString();
+        retorno.eyeColor  = root.get("eye_color").getAsString();
+        retorno.birthYear = root.get("birth_year").getAsString();
+        retorno.gender    = root.get("gender").getAsString();
+        retorno.created   = root.get("created").getAsString();
+        retorno.edited    = root.get("edited").getAsString();
 
         JsonArray films = root.getAsJsonArray("films");
 
@@ -38,8 +48,33 @@ public class PersonagemDeserializer implements JsonDeserializer<Object> {
             filmes.add(filme);
         }
 
-        retorno.filmes = filmes;
+        retorno.films = filmes;
 
         return retorno;
     }
+
+    /*
+	"homeworld": "http://swapi.co/api/planets/1/",
+	"films": [
+		"http://swapi.co/api/films/6/",
+		"http://swapi.co/api/films/3/",
+		"http://swapi.co/api/films/2/",
+		"http://swapi.co/api/films/1/",
+		"http://swapi.co/api/films/7/"
+	],
+	"species": [
+		"http://swapi.co/api/species/1/"
+	],
+	"vehicles": [
+		"http://swapi.co/api/vehicles/14/",
+		"http://swapi.co/api/vehicles/30/"
+	],
+	"starships": [
+		"http://swapi.co/api/starships/12/",
+		"http://swapi.co/api/starships/22/"
+	],
+	"created": "2014-12-09T13:50:51.644000Z",
+	"edited": "2014-12-20T21:17:56.891000Z",
+	"url": "http://swapi.co/api/people/1/"
+     */
 }
