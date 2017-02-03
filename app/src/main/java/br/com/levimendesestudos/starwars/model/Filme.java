5567b4bd4bb3ede71165b0757c67159294dc49b2
429317980 <levi.silva.mendes@gmail.com>
2017-02-03 16:58:33 -0200

    implements Serializable

--- app/src/main/java/br/com/levimendesestudos/starwars/model/Filme.java
+++ app/src/main/java/br/com/levimendesestudos/starwars/model/Filme.java
@@ -1,10 +1,12 @@
 package br.com.levimendesestudos.starwars.model;
 
+import java.io.Serializable;
+
 /**
  * Created by 809778 on 03/02/2017.
  */
 
-public class Filme {
+public class Filme implements Serializable {
 
     public int id;
     public String url;
