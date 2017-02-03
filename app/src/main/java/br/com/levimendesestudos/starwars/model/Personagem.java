b42105088417c614b4483f3aa4082a1259f54af7
429317980 <levi.silva.mendes@gmail.com>
2017-02-03 09:19:49 -0200

    adicionado a lista de filmes do personagem

--- app/src/main/java/br/com/levimendesestudos/starwars/model/Personagem.java
+++ app/src/main/java/br/com/levimendesestudos/starwars/model/Personagem.java
@@ -1,5 +1,7 @@
 package br.com.levimendesestudos.starwars.model;
 
+import java.util.List;
+
 /**
  * Created by 809778 on 02/02/2017.
  */
@@ -8,4 +10,5 @@ public class Personagem {
 
     public String link;
     public String nome;
+    public List<Filme> filmes;
 }
