f07387ebbce9391674e14eaef39f77f545bba5c3
429317980 <levi.silva.mendes@gmail.com>
2017-02-03 14:29:54 -0200

    criação dos métodos para inserir e listar Personagem

--- app/src/main/java/br/com/levimendesestudos/starwars/model/db/PersonagemDB.java
+++ app/src/main/java/br/com/levimendesestudos/starwars/model/db/PersonagemDB.java
@@ -1,6 +1,13 @@
 package br.com.levimendesestudos.starwars.model.db;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.levimendesestudos.starwars.model.Personagem;
 
 /**
  * Created by 809778 on 02/02/2017.
@@ -17,5 +24,34 @@ public class PersonagemDB extends DBGenericClass {
 
     public PersonagemDB(Context context) {
         super(context);
+    }
+
+    public boolean inserir(Personagem p) {
+        ContentValues values = new ContentValues();
+
+        values.put(NOME, p.nome);
+        values.put(LINK, p.link);
+
+        boolean retorno = database.insert(TB_NAME, null, values) > 0;
+
+        return retorno;
+    }
+
+    public List<Personagem> listar() {
+        List<Personagem> retorno = new ArrayList<>();
+        Cursor cursor = database.query(TB_NAME, null, null, null, null, null, null);
+
+        while (cursor.moveToNext()) {
+            Personagem p = new Personagem();
+
+            p.nome = cursor.getString(cursor.getColumnIndex(NOME));
+            p.link = cursor.getString(cursor.getColumnIndex(LINK));
+
+            retorno.add(p);
+        }
+
+        fecharCursor(cursor);
+
+        return retorno;
     }
 }
