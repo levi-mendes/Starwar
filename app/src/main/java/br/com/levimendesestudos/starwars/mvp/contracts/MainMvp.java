7044082f60973e695a1d801fad5abcdf451965a1
429317980 <levi.silva.mendes@gmail.com>
2017-02-03 14:28:44 -0200

    novos métodos adicionados

--- app/src/main/java/br/com/levimendesestudos/starwars/mvp/contracts/MainMvp.java
+++ app/src/main/java/br/com/levimendesestudos/starwars/mvp/contracts/MainMvp.java
@@ -1,5 +1,9 @@
 package br.com.levimendesestudos.starwars.mvp.contracts;
 
+import java.util.List;
+
+import br.com.levimendesestudos.starwars.model.Personagem;
+
 /**
  * Created by 809778 on 02/02/2017.
  */
@@ -9,10 +13,17 @@ public interface MainMvp {
     interface View {
         void configureList();
         void callCameraActivity();
+        void carregarLista(List<Personagem> list);
+        void adicionarItemNaLista(Personagem p);
+        void showLoading();
+        void showListaVazia();
+        void hideLoading();
+        void hideListaVazia();
     }
 
     interface Presenter {
         void init();
         void itemSelected(int itemId);
+        void buscarESalvar(String url);
     }
 }
