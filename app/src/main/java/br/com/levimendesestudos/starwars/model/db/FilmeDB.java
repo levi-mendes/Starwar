6a7c5f48fd57d8ad5d9a4640058ced8a28247aae
429317980 <levi.silva.mendes@gmail.com>
2017-02-03 09:22:02 -0200

    classe para manipulação da tablea TB_FILME

---
+++ app/src/main/java/br/com/levimendesestudos/starwars/model/db/FilmeDB.java
@@ -0,0 +1,19 @@
+package br.com.levimendesestudos.starwars.model.db;
+
+import android.content.Context;
+
+/**
+ * Created by 809778 on 03/02/2017.
+ */
+
+public class FilmeDB extends DBGenericClass {
+
+    public static final String TB_NAME = "";
+
+    public static final String ID  = "ID";
+    public static final String URL = "URL";
+
+    public FilmeDB(Context context) {
+        super(context);
+    }
+}
