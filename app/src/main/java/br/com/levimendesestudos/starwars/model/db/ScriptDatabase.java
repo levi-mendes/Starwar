f81cbd94cf9d129458169e3536f4ef3c19e2dbad
429317980 <levi.silva.mendes@gmail.com>
2017-02-03 14:31:43 -0200

    alteração provisória nos Script's

--- app/src/main/java/br/com/levimendesestudos/starwars/model/db/ScriptDatabase.java
+++ app/src/main/java/br/com/levimendesestudos/starwars/model/db/ScriptDatabase.java
@@ -6,13 +6,14 @@ package br.com.levimendesestudos.starwars.model.db;
 
 public class ScriptDatabase {
 
-    public static final String CREATE_TB_PERSONAGEM = "CREATE TABLE IF NOT EXISTS (" + PersonagemDB.TB_NAME  + " (" +
-            PersonagemDB.NOME      + " TEXT PRIMARY KEY NOT NULL, " +
-            PersonagemDB.LINK      + " REAL NOT NULL,             "  +
-            PersonagemDB.LATITUDE  + " REAL NOT NULL,             "  +
-            PersonagemDB.LONGITUDE + " REAL NOT NULL);";
+    public static final String CREATE_TB_PERSONAGEM = "CREATE TABLE IF NOT EXISTS " + PersonagemDB.TB_NAME  + " (" +
+            //PersonagemDB.NOME      + " TEXT PRIMARY KEY NOT NULL, " +
+            PersonagemDB.NOME      + " TEXT NOT NULL, "  +
+            PersonagemDB.LINK      + " REAL NOT NULL, "  +
+            PersonagemDB.LATITUDE  + " REAL NULL,     "  +
+            PersonagemDB.LONGITUDE + " REAL NULL);";
 
-    public static final String CREATE_TB_FILME = "CREATE TABLE IF NOT EXISTS (" + FilmeDB.TB_NAME  + " (" +
+    public static final String CREATE_TB_FILME = "CREATE TABLE IF NOT EXISTS " + FilmeDB.TB_NAME  + " (" +
             FilmeDB.ID  + " INT PRIMARY KEY NOT NULL, " +
             FilmeDB.URL + " REAL NOT NULL);";
 
