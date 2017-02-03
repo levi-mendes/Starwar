dda752c8870c3b9098fb7b32bc1e958d5961ec43
429317980 <levi.silva.mendes@gmail.com>
2017-02-03 14:20:35 -0200

    criação de tela de detalhes

---
+++ app/src/main/java/br/com/levimendesestudos/starwars/activities/DetalhesActivity.java
@@ -0,0 +1,15 @@
+package br.com.levimendesestudos.starwars.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.levimendesestudos.starwars.R;
+
+public class DetalhesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhes);
+    }
+}
