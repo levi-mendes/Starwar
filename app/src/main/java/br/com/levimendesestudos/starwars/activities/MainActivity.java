05518aff871baea269b6490c445ea0bba111829d
levimendes <levi.silva.mendes@gmail.com>
2017-02-05 21:13:28 -0200

    criação de métodos para requisitar e verificar permissões

--- app/src/main/java/br/com/levimendesestudos/starwars/activities/MainActivity.java
+++ app/src/main/java/br/com/levimendesestudos/starwars/activities/MainActivity.java
@@ -1,7 +1,11 @@
 package br.com.levimendesestudos.starwars.activities;
 
+import android.Manifest;
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -11,6 +15,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import org.greenrobot.eventbus.Subscribe;
 import java.util.List;
 import br.com.levimendesestudos.starwars.R;
@@ -19,8 +25,10 @@ import br.com.levimendesestudos.starwars.model.Personagem;
 import br.com.levimendesestudos.starwars.mvp.contracts.MainMvp;
 import br.com.levimendesestudos.starwars.mvp.presenter.MainPresenter;
 import br.com.levimendesestudos.starwars.utils.EventBusUtil;
+import br.com.levimendesestudos.starwars.utils.ToastUtil;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
 
 public class MainActivity extends BaseActivity implements MainMvp.View {
 
@@ -55,6 +63,13 @@ public class MainActivity extends BaseActivity implements MainMvp.View {
     }
 
     @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        mPresenter.requestPermissionsResult(requestCode, grantResults);
+    }
+
+    @Override
     public void carregarLista(List<Personagem> list) {
         mAdapter = new ListaPersonagensAdapter(this, list);
         rvPersonagens.setAdapter(mAdapter);
@@ -80,11 +95,9 @@ public class MainActivity extends BaseActivity implements MainMvp.View {
     }
 
     /**
-     *
      * metodo chamado apos a leitura do QRCode
      *
      * @param url
-     *
      */
     @Subscribe
     public void qrcode(String url) {
@@ -94,6 +107,7 @@ public class MainActivity extends BaseActivity implements MainMvp.View {
 
     /**
      * Adiciona o novo Personagem  na lista
+     *
      * @param p
      */
     @Override
@@ -135,5 +149,43 @@ public class MainActivity extends BaseActivity implements MainMvp.View {
     @Override
     public void hideListaVazia() {
         tvListaVazia.setVisibility(View.GONE);
+    }
+
+    @Override
+    public boolean accessFineLocation() {
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PERMISSION_GRANTED;
+    }
+
+    @Override
+    public boolean camera() {
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PERMISSION_GRANTED;
+    }
+
+    @Override
+    public boolean writeExternalStorage() {
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PERMISSION_GRANTED;
+    }
+
+    @Override
+    public void finalizar() {
+        finish();
+    }
+
+    @Override
+    @TargetApi(Build.VERSION_CODES.M) //Android sdk = 23 ou superior
+    public void requesWriteExternalStorage() {
+        requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_WRITE_EXTERNAL_STORAGE_PERMISSION);
+    }
+
+    @Override
+    @TargetApi(Build.VERSION_CODES.M) //Android sdk = 23 ou superior
+    public void requesAccessFineLocation() {
+        requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_ACCES_FINE_LOCATION_PERMISSION);
+    }
+
+    @Override
+    @TargetApi(Build.VERSION_CODES.M) //Android sdk = 23 ou superior
+    public void requesCamera() {
+        requestPermissions(new String[]{Manifest.permission.CAMERA}, REQUEST_ACCES_FINE_LOCATION_PERMISSION);
     }
 }
